97191619209c47c754f4c9e8231e6aebed6e951b
Michael Snoyman <michael@snoyman.com>
2018-06-25 17:51:58 +0300

    Make it work again

--- src/GitHash.hs
+++ src/GitHash.hs
@@ -104,96 +104,85 @@ giCommitCount :: GitInfo -> Int
 giCommitCount = _giCommitCount
 
 -- | Get the 'GitInfo' for the given root directory. Root directory
--- should be the @.git@ directory itself.
+-- should be the directory containing the @.git@ directory.
 --
 -- @since 0.1.0.0
 getGitInfo :: FilePath -> IO (Either GitHashException GitInfo)
 getGitInfo root = try $ do
-  mgit <- findExecutable "git"
-  case mgit of
-    Nothing -> throwIO GHEGitNotFound
-    Just gitpath -> do
-      -- a lot of bookkeeping to record the right dependencies
-      let hd         = root </> "HEAD"
-          index      = root </> "index"
-          packedRefs = root </> "packed-refs"
-      ehdRef <- try $ B.readFile hd
-      files1 <-
-        case ehdRef of
-          Left e
-            | isDoesNotExistError e -> return []
-            | otherwise -> throwIO $ GHECouldn'tReadFile hd e
-          Right hdRef -> do
-            -- the HEAD file either contains the hash of a detached head
-            -- or a pointer to the file that contains the hash of the head
-            case B.splitAt 5 hdRef of
-              -- pointer to ref
-              ("ref: ", relRef) -> do
-                let ref = root </> B8.unpack relRef
-                refExists <- doesFileExist ref
-                return $ if refExists then [ref] else []
-              -- detached head
-              _hash -> return [hd]
-      -- add the index if it exists to set the dirty flag
-      indexExists <- doesFileExist index
-      let files2 = if indexExists then [index] else []
-      -- if the refs have been packed, the info we're looking for
-      -- might be in that file rather than the one-file-per-ref case
-      -- handled above
-      packedExists <- doesFileExist packedRefs
-      let files3 = if packedExists then [packedRefs] else []
+  -- a lot of bookkeeping to record the right dependencies
+  let hd         = root </> "HEAD"
+      index      = root </> "index"
+      packedRefs = root </> "packed-refs"
+  ehdRef <- try $ B.readFile hd
+  files1 <-
+    case ehdRef of
+      Left e
+        | isDoesNotExistError e -> return []
+        | otherwise -> throwIO $ GHECouldn'tReadFile hd e
+      Right hdRef -> do
+        -- the HEAD file either contains the hash of a detached head
+        -- or a pointer to the file that contains the hash of the head
+        case B.splitAt 5 hdRef of
+          -- pointer to ref
+          ("ref: ", relRef) -> do
+            let ref = root </> B8.unpack relRef
+            refExists <- doesFileExist ref
+            return $ if refExists then [ref] else []
+          -- detached head
+          _hash -> return [hd]
+  -- add the index if it exists to set the dirty flag
+  indexExists <- doesFileExist index
+  let files2 = if indexExists then [index] else []
+  -- if the refs have been packed, the info we're looking for
+  -- might be in that file rather than the one-file-per-ref case
+  -- handled above
+  packedExists <- doesFileExist packedRefs
+  let files3 = if packedExists then [packedRefs] else []
 
-          _giFiles = concat [files1, files2, files3]
-          run args = do
-            let cp = (proc gitpath args)
-                  { cwd = Just root
-                  , std_err = NoStream
-                  }
-            eres <- try $ readCreateProcess cp ""
-            case eres of
-              Left e -> throwIO $ GHEGitRunFailed root args e
-              Right str -> return $ takeWhile (/= '\n') str
+      _giFiles = concat [files1, files2, files3]
+      run args = do
+        eres <- runGit root args
+        case eres of
+          Left e -> throwIO e
+          Right str -> return $ takeWhile (/= '\n') str
 
-      _giHash <- run ["rev-parse", "HEAD"]
-      _giBranch <- run ["rev-parse", "--abbrev-ref", "HEAD"]
+  _giHash <- run ["rev-parse", "HEAD"]
+  _giBranch <- run ["rev-parse", "--abbrev-ref", "HEAD"]
 
-      dirtyString <- run ["status", "--porcelain"]
-      let _giDirty = not $ null (dirtyString :: String)
+  dirtyString <- run ["status", "--porcelain"]
+  let _giDirty = not $ null (dirtyString :: String)
 
-      commitCount <- run ["rev-list", "HEAD", "--count"]
-      _giCommitCount <-
-        case readMaybe commitCount of
-          Nothing -> throwIO $ GHEInvalidCommitCount root commitCount
-          Just x -> return x
+  commitCount <- run ["rev-list", "HEAD", "--count"]
+  _giCommitCount <-
+    case readMaybe commitCount of
+      Nothing -> throwIO $ GHEInvalidCommitCount root commitCount
+      Just x -> return x
 
-      _giCommitDate <- run ["log", "HEAD", "-1", "--format=%cd"]
+  _giCommitDate <- run ["log", "HEAD", "-1", "--format=%cd"]
 
-      return GitInfo {..}
+  return GitInfo {..}
 
 -- | Get the root directory of the Git repo containing the given file
 -- path.
 --
 -- @since 0.1.0.0
 getGitRoot :: FilePath -> IO (Either GitHashException FilePath)
-getGitRoot dir = do
-    let cp = (proc "git" args)
-          { cwd = Just dir
-          , std_err = NoStream
-          }
-        args = ["rev-parse", "--show-toplevel"]
-    eres <- try $ readCreateProcess cp ""
-    return $ case eres of
-      Left e -> Left $ GHEGitRunFailed dir args e
-      Right out -> Right $ takeWhile (/= '\n') out </> ".git"
+getGitRoot dir = fmap (takeWhile (/= '\n')) `fmap` (runGit dir ["rev-parse", "--show-toplevel"])
+
+runGit root args = do
+  let cp = (proc "git" args) { cwd = Just root }
+  (ec, out, err) <- readCreateProcessWithExitCode cp ""
+  case ec of
+    ExitSuccess -> return $ Right out
+    ExitFailure _ -> return $ Left $ GHEGitRunFailed root args ec out err
 
 -- | Exceptions which can occur when using this library's functions.
 --
 -- @since 0.1.0.0
 data GitHashException
-  = GHEGitNotFound
-  | GHECouldn'tReadFile !FilePath !IOException
+  = GHECouldn'tReadFile !FilePath !IOException
   | GHEInvalidCommitCount !FilePath !String
-  | GHEGitRunFailed !FilePath ![String] !IOException
+  | GHEGitRunFailed !FilePath ![String] !ExitCode !String !String
   deriving (Show, Typeable)
 instance Exception GitHashException
 
